34106a1f62f3d90abd438e9240547c22271f25e6
abeni-al7 <abenezeralebachew3@gmail.com>
2025-01-14 21:47:30 +0300

    sorting

---
+++ sorting_algos.cpp
@@ -0,0 +1,32 @@
+# include <iostream>
+using namespace std;
+
+
+void shell_sort(int arr[], int n) {
+    for (int gap = n/2; gap > 0; gap /= 2) {
+        for (int i = gap; i < n; i++) {
+            int temp = arr[i];
+            int j;
+            for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
+                arr[j] = arr[j - gap];
+            }
+            arr[j] = temp;
+        }
+    }
+}
+
+void quick_sort(int arr[], int low, int high) {
+    if (low < high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(arr[i], arr[j]);
+            }
+        }
+        swap(arr[i + 1], arr[high]);
+        quick_sort(arr, low, i);
+        quick_sort(arr, i + 2, high);
+    }
+}
